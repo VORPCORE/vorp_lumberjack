ca82dab65239431a8b926f7df30547d2bdbe1213
Niklas B <68842909+justSalkin@users.noreply.github.com>
2023-02-23 17:32:48 +0100

    german translation for config file

---
+++ (German)config.lua
@@ -0,0 +1,39 @@
+----------------------
+-- DE - GERMAN Translation
+-- replace the parts in config.lua with the following translation
+----------------------
+
+
+Config.Language = {
+    chopprompt = "Hacken ",
+    choptree = "Baum fällen ",
+    putawayprompt = "Einstecken ",
+    useprompt = "Benutzen ",
+    newaxe = "Haltbarkeit = 98",
+    durability = "Haltbarkeit = ",
+    axebroken = "Deine Axt ist zerbrochen ",
+    noaxe = "Du hast keine Axt ... ",
+    nothing = "Du hast nichts erhalten ... ",
+    something = "Du erhältst ",
+    fullinventory = "Du kannst nicht mehr tragen "
+}
+
+Config.AxeFailTexts = {
+    "Idiot, pass doch auf!",
+    "Oops, da hätte ich fast meinen Fuß erwischt ...",
+    "Du sollst den Baum treffen!",
+    "Das ist eine Axt, kein Hammer!",,
+    "Verdammt, weg gerutscht!",
+    "Verdammt, daneben!",
+    "*#!$#!*",
+}
+
+Config.Items = {
+    {name = "sap", label = "Harz", chance = 8, amount = 2},
+    {name = "honey", label = "Honig", chance = 5, amount = 2},
+    {name = "wood", label = "Holz", chance = 10, amount = 5},
+    {name = "hwood", label = "Hartholz", chance = 8, amount = 5},
+    {name = "rubber", label = "Gummi", chance = 5, amount = 4},
+    {name = "branches", label = "Zweige", chance = 8, amount = 5},
+    {name = "pulp", label = "Zellstoff", chance = 10, amount = 3},
+}
