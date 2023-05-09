dd7b8bcf4d0c689c4b2faa6e53f4144c1a67f179
cleisson <cleisson@outlook.com.br>
2023-05-09 01:01:49 -0300

    Improving identing code and translation

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,16 +2,19 @@ description 'Vorp Lumberjack'
 
 fx_version "adamant"
 
-games {"rdr3"}
+games { "rdr3" }
 
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 client_scripts {
-   'config.lua',
    'client/client.lua'
 }
 
-server_scripts {
+shared_scripts {
    'config.lua',
+   'shared/*.lua'
+}
+
+server_scripts {
    'server/server.lua'
 }
