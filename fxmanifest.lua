043a5917303352bfc786c7c02dbdf77a340cc3cf
cleisson <cleisson@outlook.com.br>
2023-05-09 01:05:25 -0300

    Adding version check

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -18,3 +18,12 @@ shared_scripts {
 server_scripts {
    'server/server.lua'
 }
+
+dependencies {
+   'syn_minigame'
+}
+
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/vorp_lumberjack'
